7826c04809f8bdeb4bf2497994c892749cd4fe42
Dennis <dennis.briefkasten@gmail.com>
2026-05-19 14:43:35 +0200

    Revert "fix: read DownloadBasePath from config in DI registration"
    This reverts commit 7576ff7f34bd4e41c65f14f77e74fc30942cf545.

--- src/Paperoni.Contract/DependencyInjection.cs
+++ src/Paperoni.Contract/DependencyInjection.cs
@@ -5,11 +5,12 @@ namespace Paperoni.Contract;
 
 public static class DependencyInjection
 {
-    public static WorkingDirectory AddPaperoniWorkingDirectory(this IServiceCollection collection, IConfiguration configuration)
+    public static WorkingDirectory AddPaperoniWorkingDirectory(this IServiceCollection collection,
+        IConfiguration configuration)
     {
-        var downloadBasePath = configuration["DownloadBasePath"];
-        var workingDir = new WorkingDirectory { PaperoniWorkingDirectory = downloadBasePath };
-        Console.WriteLine($" > Album Working Directory: {workingDir.BasePath}");
+        var paperoniWorkingDirectory = configuration["PaperoniWorkingDirectory"];
+        var workingDir = new WorkingDirectory { PaperoniWorkingDirectory = paperoniWorkingDirectory };
+        Console.WriteLine($" > Working Directory: {workingDir.BasePath}");
         collection.AddSingleton(workingDir);
         return workingDir;
     }
